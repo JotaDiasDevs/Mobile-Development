e6218589224f7c321c0bcad1e0ec8b555866f29d
JotaDiasDevs <joaovitor.semente.d@gmail.com>
2026-02-12 22:42:43 -0300

    Exercicio 5.2 - Adiciona estrutura de controle switch para exibir o dia da semana

--- exercicio-1-1.js
+++ exercicio-1-1.js
@@ -113,3 +113,33 @@ if (nota >= 7) {
     console.log("Reprovado");
 }
 
+console.log(" ");
+
+let diaSemana = 4; 
+
+switch (diaSemana) {
+    case 1:
+        console.log("Segunda-feira");
+        break;
+    case 2:
+        console.log("Terça-feira");
+        break;
+    case 3:
+        console.log("Quarta-feira");
+        break;
+    case 4:
+        console.log("Quinta-feira");
+        break;
+    case 5:
+        console.log("Sexta-feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+    default:
+        console.log("Dia inválido.");
+}
+
