4e6b61a2c48d808b74abef3a039f010efc3b1627
JotaDiasDevs <joaovitor.semente.d@gmail.com>
2026-02-12 22:06:10 -0300

    exercicio 1.2 - realizamos os calculos com o console log

--- exercicio-1-1.js
+++ exercicio-1-1.js
@@ -1,3 +1,12 @@
 console.log("Olá, sou o João Victor Semente Dias.");
 console.log("Estou cursando Análise e Desenvolvimento de Sistemas na FIAP.");
 console.log("Estou aprendendo JavaScript!");
+
+console.log(" ");
+
+console.log( 100 / 4 );
+console.log( 7 * 7 );
+console.log( 17 % 5);
+
+console.log(" ");
+
